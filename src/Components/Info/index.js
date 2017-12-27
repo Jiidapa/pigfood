9b33e848499ba6b693a33c65950a4f999d58927f
jiidapa <jidapa.p@kkumail.com>
2017-12-27 17:40:52 +0700

    jida edit

--- src/Components/Info/index.js
+++ src/Components/Info/index.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import { Container, Header, Content, Button } from 'native-base';
+import { Actions } from 'react-native-router-flux';
 class Info extends Component {
+    linkToHome() {
+        Actions.home();
+    }
     render() {
         return (
             <View>
                 <Text> Hello </Text>
+                <Button onPress={() => this.linkToHome()}>
+                    <Text>Click Me! </Text>
+                </Button>
             </View>
         );
     }
