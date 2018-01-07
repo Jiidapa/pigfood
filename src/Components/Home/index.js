e98c2eeb59c14fc082b914aedd863f842ce1c7b2
dreamteryst <dreamt451@email.com>
2018-01-07 20:04:33 +0700

    new design home

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { View, Image, ImageBackground, StyleSheet, TouchableOpacity } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import {
     Container,
+    Content,
     Button,
     Header,
     Text,
@@ -9,10 +11,14 @@ import {
     Icon,
     Right,
     Body,
-    Title
+    Title,
+    Card,
+    CardItem
 } from 'native-base';
+import { Col, Row, Grid } from "react-native-easy-grid";
 import { Actions } from 'react-native-router-flux';
 
+const bg_menu = require('../../../assets/img/menu/menu1_01.png');
 const bg_menu_1 = require('../../../assets/img/menu/menu1_02.png');
 const bg_menu_2 = require('../../../assets/img/menu/menu1_03.png');
 const bg_menu_3 = require('../../../assets/img/menu/menu1_04.png');
@@ -23,37 +29,51 @@ class Home extends Component {
     render() {
         return (
             <Container>
-                <Header style={{ backgroundColor: '#1E88E5' }}>
-                    {/* <Left>
-                        <Button transparent>
-                            <Icon name='arrow-back' />
-                        </Button>
-                    </Left> */}
-                    <Body>
-                        <Title style={styles.kanit}>เครื่องผสมอาหารสุกร</Title>
-                    </Body>
-                </Header>
-                <TouchableOpacity style={{ flex: 1 }} onPress={() => Actions.info()}>
-                    <View style={[{ flex: 1 }, styles.box]}>
-                        <Text style={styles.kanit}>ความรู้สุกร</Text>
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity style={{ flex: 1 }} onPress={() => Actions.farms()}>
-                    <View style={[{ flex: 1 }, styles.box]}>
-                        <Text style={styles.kanit}>ผสมอาหาร</Text>
-                    </View>
-                </TouchableOpacity>
-                <View style={{ flex: 1, flexDirection: 'row' }}>
-                    <TouchableOpacity style={{ flex: 1 }}>
-                        <View style={[{ flex: 1 }, styles.box]}>
-                            <Text style={styles.kanit}>วิธีใช้</Text>
+                <View style={{ flex: 1 }}>
+                    <ImageBackground source={bg_menu_1} style={[styles.header, styles.centered]}>
+                        <View style={[styles.centered, styles.headerView]}>
+                            
                         </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={{ flex: 1 }}>
-                        <View style={[{ flex: 1 }, styles.box]}>
-                            <Text style={styles.kanit}>เกี่ยวกับเรา</Text>
+                        <View style={{ position: 'absolute' }}>
+                        <Text style={[styles.textPrimary, { fontSize: 26 }]}>Food{' '}
+                            <Text style={{ color: '#00B0FF', fontSize: 26}}>Mixer</Text>{' '}
+                            Machine
+                        </Text>
+                        <Text style={[styles.textPrimary, { fontSize: 14 }]}>เครื่องผสมอาหารสุกรอัตโนมัติ</Text>
                         </View>
-                    </TouchableOpacity>
+                    </ImageBackground>
+                </View>
+                <View style={{ backgroundColor: '#fff', flexDirection: 'row' }}>
+                    <Grid>
+                        <Col style={{ backgroundColor: '#fff', height: 200, borderRightWidth: 0.5, borderColor: '#ddd' }}>
+                            <Row style={[styles.box, styles.centered, styles.boxPrimary]}>
+                                <TouchableOpacity style={{ alignItems: 'center' }} onPress={() => Actions.farms()}>
+                                    <FontAwesome name="android" style={[styles.icon, styles.textPrimary]}></FontAwesome>
+                                    <Text style={[styles.textPrimary, styles.kanit]}>ผสมอาหาร</Text>
+                                </TouchableOpacity>
+                            </Row>
+                            <Row style={[styles.box, styles.centered]}>
+                                <TouchableOpacity style={{ alignItems: 'center' }}>
+                                    <FontAwesome name="info-circle" style={[styles.icon]}></FontAwesome>
+                                    <Text>วิธีใช้</Text>
+                                </TouchableOpacity>
+                            </Row>
+                        </Col>
+                        <Col style={{ backgroundColor: '#fff', height: 200, borderRightWidth: 0.5, borderColor: '#ddd' }}>
+                            <Row style={[styles.box, styles.centered]}>
+                                <TouchableOpacity style={{ alignItems: 'center' }} onPress={() => Actions.info()}>
+                                    <FontAwesome name="book" style={[styles.icon]}></FontAwesome>
+                                    <Text>ความรู้เรื่องสุกร</Text>
+                                </TouchableOpacity>
+                            </Row>
+                            <Row style={[styles.box, styles.centered]}>
+                                <TouchableOpacity style={{ alignItems: 'center' }}>
+                                    <FontAwesome name="question-circle" style={[styles.icon]}></FontAwesome>
+                                    <Text>เกี่ยวกับเรา</Text>
+                                </TouchableOpacity>
+                            </Row>
+                        </Col>
+                    </Grid>
                 </View>
             </Container>
         );
@@ -61,9 +81,34 @@ class Home extends Component {
 }
 
 const styles = StyleSheet.create({
+    header: {
+        height: 150,
+        width: null,
+        flexDirection: 'row'
+    },
+    headerView: {
+        height: 75,
+        flex: 1,
+        backgroundColor: '#000',
+        opacity: 0.6
+    },
     box: {
+        padding: 8,
+        borderBottomWidth: 0.5,
+        borderColor: '#ddd'
+    },
+    boxPrimary: {
+        backgroundColor: '#00B0FF',
+    },
+    centered: {
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    icon: {
+        fontSize: 40
+    },
+    textPrimary: {
+        color: 'white'
     },
     kanit: {
         fontFamily: 'Kanit-Regular'
