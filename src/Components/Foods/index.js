49525c6c1f11d4a014229a52d8dbcac74a8bbb43
jiidapa <jidapa.p@kkumail.com>
2018-01-14 17:40:22 +0700

    upload image success

--- src/Components/Foods/index.js
+++ src/Components/Foods/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { Actions } from 'react-native-router-flux';
 import {MyHeader} from '../common';
 import styles from '../styles';
 class Foods extends Component {
