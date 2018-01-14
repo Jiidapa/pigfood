ebaed09ad48dd896011d96c48583faf197549ea5
SUKITZ\SUKITz <sukit@kkumail.com>
2018-01-14 14:28:50 +0700

    pigImg

--- src/Components/Pigs/index.js
+++ src/Components/Pigs/index.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { Actions } from 'react-native-router-flux';
-import { View, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, ImageBackground, StyleSheet } from 'react-native';
 import { Container, Content, Text, Card, CardItem } from 'native-base';
 import { MyHeader } from '../common';
 import styles from '../styles';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import Swipeout from 'react-native-swipeout';
 
+const pig01 = require('../../../assets/img/pigs/pig01.png');
+const pig02 = require('../../../assets/img/pigs/pig02.png');
+const pig03 = require('../../../assets/img/pigs/pig03.png');
+const pig04 = require('../../../assets/img/pigs/pig04.png');
+const pig05 = require('../../../assets/img/pigs/pig05.png');
 
 let swipeBtns = [
     {
@@ -35,32 +40,86 @@ class Pigs extends Component {
                 <Content>
                     <Grid>
                         <Col>
-                        <Swipeout right={swipeBtns} backgroundColor= 'transparent'>
-                        <TouchableOpacity onPress={() => Actions.Foods()}>
-                            <Row>
-                                <Card>
-                                    <CardItem>
-                                            <View style={{paddingLeft: 20}}>
-                                                <Text>ประเภท : หมูอาย</Text>
-                                                <Text>จำนวน : 1 ตัว</Text>
-                                            </View>
-                                    </CardItem>
-                                </Card>
-                            </Row>
-                            </TouchableOpacity>
-                        </Swipeout>
-                            <TouchableOpacity>
-                            <Row>
-                                <Card>
-                                    <CardItem>
-                                        <View style={{paddingLeft: 20}}>
-                                            <Text>ประเภท : หมูดรีม</Text>
-                                            <Text>จำนวน : 1 ตัว</Text>
-                                        </View>
-                                    </CardItem>
-                                </Card>
-                            </Row>
-                            </TouchableOpacity>
+                            <Swipeout right={swipeBtns} backgroundColor= 'transparent'  style={{marginTop:15}}>
+                                <TouchableOpacity onPress={() => Actions.Foods()}>
+                                    <Row>
+                                        {/* <Card> */}
+                                            <ImageBackground source={pig01} style={{ width: null, height: null,flex: 1}} >
+                                                <CardItem style={{backgroundColor:'transparent'}}>
+                                                        <View style={{paddingLeft: 20}}>
+                                                            <Text>ประเภท : หมูอาย</Text>
+                                                            <Text>จำนวน : 1 ตัว</Text>
+                                                        </View>
+                                                </CardItem>
+                                            </ImageBackground>
+                                        {/* </Card> */}
+                                    </Row>
+                                </TouchableOpacity>
+                            </Swipeout>
+                            <Swipeout right={swipeBtns} backgroundColor= 'transparent' >
+                                <TouchableOpacity onPress={() => Actions.Foods()}>
+                                    <Row>
+                                        {/* <Card> */}
+                                            <ImageBackground source={pig02} style={{ width: null, height: null,flex: 1}} >
+                                                <CardItem style={{backgroundColor:'transparent'}}>
+                                                        <View style={{paddingLeft: 20}}>
+                                                            <Text>ประเภท : หมูอาย</Text>
+                                                            <Text>จำนวน : 2 ตัว</Text>
+                                                        </View>
+                                                </CardItem>
+                                            </ImageBackground>
+                                        {/* </Card> */}
+                                    </Row>
+                                </TouchableOpacity>
+                            </Swipeout>
+                            <Swipeout right={swipeBtns} backgroundColor= 'transparent' >
+                                <TouchableOpacity onPress={() => Actions.Foods()}>
+                                    <Row>
+                                        {/* <Card> */}
+                                            <ImageBackground source={pig03} style={{ width: null, height: null,flex: 1}} >
+                                                <CardItem style={{backgroundColor:'transparent'}}>
+                                                        <View style={{paddingLeft: 20}}>
+                                                            <Text>ประเภท : หมูอาย</Text>
+                                                            <Text>จำนวน : 3 ตัว</Text>
+                                                        </View>
+                                                </CardItem>
+                                            </ImageBackground>
+                                        {/* </Card> */}
+                                    </Row>
+                                </TouchableOpacity>
+                            </Swipeout>
+                            <Swipeout right={swipeBtns} backgroundColor= 'transparent' >
+                                <TouchableOpacity onPress={() => Actions.Foods()}>
+                                    <Row>
+                                        {/* <Card> */}
+                                            <ImageBackground source={pig04} style={{ width: null, height: null,flex: 1}} >
+                                                <CardItem style={{backgroundColor:'transparent'}}>
+                                                        <View style={{paddingLeft: 20}}>
+                                                            <Text>ประเภท : หมูอาย</Text>
+                                                            <Text>จำนวน : 4 ตัว</Text>
+                                                        </View>
+                                                </CardItem>
+                                            </ImageBackground>
+                                        {/* </Card> */}
+                                    </Row>
+                                </TouchableOpacity>
+                            </Swipeout>
+                            <Swipeout right={swipeBtns} backgroundColor= 'transparent'>
+                                <TouchableOpacity onPress={() => Actions.Foods()}>
+                                    <Row>
+                                        {/* <Card> */}
+                                            <ImageBackground source={pig05} style={{ width: null, height: null,flex: 1}} >
+                                                <CardItem style={{backgroundColor:'transparent'}}>
+                                                        <View style={{paddingLeft: 20}}>
+                                                            <Text>ประเภท : หมูอาย</Text>
+                                                            <Text>จำนวน : 5 ตัว</Text>
+                                                        </View>
+                                                </CardItem>
+                                            </ImageBackground>
+                                        {/* </Card> */}
+                                    </Row>
+                                </TouchableOpacity>
+                            </Swipeout>
                         </Col>
                     </Grid>
                 </Content>
