13a8717311511f4ff17b513c71d82abf4d9a7ddc
dreamteryst <dreamt451@gmail.com>
2018-01-14 14:28:17 +0700

    can added farm

--- src/Components/Farms/add.js
+++ src/Components/Farms/add.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Image, TouchableOpacity, Platform } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { Actions } from 'react-native-router-flux';
 import ImagePicker from 'react-native-image-picker';
@@ -15,13 +15,47 @@ import {
     Label,
     Button
 } from 'native-base';
+import RNFetchBlob from 'react-native-fetch-blob';
 import { MyHeader, Cover } from '../common';
 import { nameChange, pictureChange } from '../../Actions';
 import styles from '../styles';
 
+// Prepare Blob support
+const Blob = RNFetchBlob.polyfill.Blob;
+const fs = RNFetchBlob.fs;
+window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest;
+window.Blob = Blob;
 const BGMenu = require('../../../assets/img/menu/menu1_04.png');
 
 class AddFarm extends Component {
+
+    uploadImage(uri, mime = 'application/octet-stream') {
+        return new Promise((resolve, reject) => {
+          const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri;
+          let uploadBlob = null;
+    
+          const imageRef = firebase.storage().ref('images').child('farms');
+    
+          fs.readFile(uploadUri, 'base64')
+            .then((data) => {
+              return Blob.build(data, { type: `${mime};BASE64` });
+            })
+            .then((blob) => {
+              uploadBlob = blob;
+              return imageRef.put(blob, { contentType: mime });
+            })
+            .then(() => {
+              uploadBlob.close();
+              return imageRef.getDownloadURL();
+            })
+            .then((url) => {
+              resolve(url);
+            })
+            .catch((error) => {
+              reject(error);
+          });
+        });
+      }
 
     show() {
         ImagePicker.showImagePicker((response) => {
@@ -34,11 +68,12 @@ class AddFarm extends Component {
             } else if (response.customButton) {
                 console.log('User tapped custom button: ', response.customButton);
             } else {
-                const source = { uri: response.uri };
-
-                // You can also display the image using data:
-                // let source = { uri: 'data:image/jpeg;base64,' + response.data };
-                this.props.pictureChange(source);
+                this.uploadImage(response.uri)
+                .then(url => { 
+                    alert('uploaded'); 
+                    this.props.pictureChange(url);
+                })
+                .catch(error => console.log(error));
             }
         });
     }
